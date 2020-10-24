a7ba3c052b5afedf761c61e1d2147b37cf767324
Mulang Su <sumulang@gmail.com>
2020-10-24 09:58:17 +0800

    added simple table view boilerplate

---
+++ PDFCreator/PDFListViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+import RealmSwift
+
+class PDFListViewController: UITableViewController {
+
+    var pdfs: Results<PDFFileObject>!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pdfs = DataManager.shared.pdfs
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        pdfs.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = pdfs[indexPath.row].fileName
+        return cell
+    }
+}
+
