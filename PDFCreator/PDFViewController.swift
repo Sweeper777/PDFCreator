34a0e71d9e8db98e78bead778a8352f25ea1f297
Mulang Su <sumulang@gmail.com>
2020-11-27 22:03:54 +0800

    set up pdfView and title

--- PDFCreator/PDFViewController.swift
+++ PDFCreator/PDFViewController.swift
@@ -6,5 +6,12 @@ class PDFViewController : UIViewController {
     var pageIndex: Int!
 
     override func viewDidLoad() {
+        let pdfView = PDFView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        self.view = pdfView
+        pdfView.document = document
+        pdfView.displayMode = .singlePage
+        pdfView.go(to: document.page(at: pageIndex)!)
+
+        title = "Page \(pageIndex + 1)"
     }
 }
