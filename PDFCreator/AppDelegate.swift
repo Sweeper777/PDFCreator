870f6c8656fe872da022ccf1bc7980933d7c96d0
Mulang Su <sumulang@gmail.com>
2021-12-05 12:04:26 +0000

    apparently iOS 15 breaks my navbar

--- PDFCreator/AppDelegate.swift
+++ PDFCreator/AppDelegate.swift
@@ -14,6 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if #available(iOS 15, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            appearance.backgroundColor = UIColor(hex: "3b7b3b")
+            UINavigationBar.appearance().standardAppearance = appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        }
         return true
     }
 
